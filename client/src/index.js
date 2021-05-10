a174c0067e5036b6cd8b3dc5b212c33cd4abc314
Hossam Khalil <hossamkhalil001@gmail.com>
2021-05-10 21:44:53 +0200

    Feat(file-structure): added frontend ui dependcies

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.js";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
