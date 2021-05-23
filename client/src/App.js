6de45e65f985d1301d0fc1c06d9f6532d0749956
Hossam Khalil <hossamkhalil001@gmail.com>
2021-05-23 23:56:56 +0200

    feature(layout): created landing page template

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,18 @@
 import "./App.css";
+import Footer from "./components/layouts/Footer";
+import NavBar from "./components/layouts/NavBar";
 import Router from "./components/Router";
 import { checkTokenValid } from "./services/authService";
+
+
 function App() {
 
   checkTokenValid();
   return (
     <div className="App">
+      <NavBar />
       <Router />
+      <Footer />
     </div >
   );
 }
--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,10 +1,174 @@
-import { Grid } from "@material-ui/core";
-import Logout from "../components/Logout";
-export default function landing() {
+
+
+export default function LandingPage() {
   return (
-    <Grid container alignItems="flex-start" justify="center" spacing={2}>
-      <h1>landing</h1>
-      <Logout />
-    </Grid>
+    // <Grid container alignItems="flex-start" justify="center" spacing={2}>
+    //   <h1>landing</h1>
+    //   <Logout />
+    // </Grid>
+    <div>
+      <section id="hero" className="d-flex justify-content-center align-items-center" >
+        <div className="container position-relative" data-aos="zoom-in" data-aos-delay="100">
+          <h1>Learning Today,<br />Leading Tomorrow</h1>
+          <h2>We are team of talented designers making websites with Bootstrap</h2>
+          <a href="courses.html" className="btn-get-started">Get Started</a>
+        </div>
+      </section >
+
+      <section id="popular-courses" className="courses">
+        <div className="container" data-aos="fade-up">
+
+          <div className="section-title">
+            <h2>Courses</h2>
+            <p>Popular Courses</p>
+          </div>
+
+          <div className="row" data-aos="zoom-in" data-aos-delay="100">
+
+            <div className="col-lg-4 col-md-6 d-flex align-items-stretch">
+              <div className="course-item">
+                <img src="assets/img/course-1.jpg" className="img-fluid" alt="..." />
+                <div className="course-content">
+                  <div className="d-flex justify-content-between align-items-center mb-3">
+                    <h4>Web Development</h4>
+                    <p className="price">$169</p>
+                  </div>
+
+                  <h3><a href="course-details.html">Website Design</a></h3>
+                  <p>Et architecto provident deleniti facere repellat nobis iste. Id facere quia quae dolores dolorem tempore.</p>
+                  <div className="trainer d-flex justify-content-between align-items-center">
+                    <div className="trainer-profile d-flex align-items-center">
+                      <img src="assets/img/trainers/trainer-1.jpg" className="img-fluid" alt="" />
+                      <span>Antonio</span>
+                    </div>
+                    <div className="trainer-rank d-flex align-items-center">
+                      <i className="bx bx-user"></i>&nbsp;50&nbsp;&nbsp;
+                      <i className="bx bx-heart"></i>&nbsp;65
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-md-0">
+              <div className="course-item">
+                <img src="assets/img/course-2.jpg" className="img-fluid" alt="..." />
+                <div className="course-content">
+                  <div className="d-flex justify-content-between align-items-center mb-3">
+                    <h4>Marketing</h4>
+                    <p className="price">$250</p>
+                  </div>
+
+                  <h3><a href="course-details.html">Search Engine Optimization</a></h3>
+                  <p>Et architecto provident deleniti facere repellat nobis iste. Id facere quia quae dolores dolorem tempore.</p>
+                  <div className="trainer d-flex justify-content-between align-items-center">
+                    <div className="trainer-profile d-flex align-items-center">
+                      <img src="assets/img/trainers/trainer-2.jpg" className="img-fluid" alt="" />
+                      <span>Lana</span>
+                    </div>
+                    <div className="trainer-rank d-flex align-items-center">
+                      <i className="bx bx-user"></i>&nbsp;35
+                      &nbsp;&nbsp;
+                      <i className="bx bx-heart"></i>&nbsp;42
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-lg-0">
+              <div className="course-item">
+                <img src="assets/img/course-3.jpg" className="img-fluid" alt="..." />
+                <div className="course-content">
+                  <div className="d-flex justify-content-between align-items-center mb-3">
+                    <h4>Content</h4>
+                    <p className="price">$180</p>
+                  </div>
+
+                  <h3><a href="course-details.html">Copywriting</a></h3>
+                  <p>Et architecto provident deleniti facere repellat nobis iste. Id facere quia quae dolores dolorem tempore.</p>
+                  <div className="trainer d-flex justify-content-between align-items-center">
+                    <div className="trainer-profile d-flex align-items-center">
+                      <img src="assets/img/trainers/trainer-3.jpg" className="img-fluid" alt="" />
+                      <span>Brandon</span>
+                    </div>
+                    <div className="trainer-rank d-flex align-items-center">
+                      <i className="bx bx-user"></i>&nbsp;20
+                          &nbsp;&nbsp;
+                          <i className="bx bx-heart"></i>&nbsp;85
+                  </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </section>
+
+      <section id="trainers" className="trainers">
+        <div className="container" data-aos="fade-up">
+          <div className="row" data-aos="zoom-in" data-aos-delay="100">
+            <div className="col-lg-4 col-md-6 d-flex align-items-stretch">
+              <div className="member">
+                <img src="assets/img/trainers/trainer-1.jpg" className="img-fluid" alt="" />
+                <div className="member-content">
+                  <h4>Walter White</h4>
+                  <span>Web Development</span>
+                  <p>
+                    Magni qui quod omnis unde et eos fuga et exercitationem. Odio veritatis perspiciatis quaerat qui aut aut aut
+                  </p>
+                  <div className="social">
+                    <a href=""><i className="bi bi-twitter"></i></a>
+                    <a href=""><i className="bi bi-facebook"></i></a>
+                    <a href=""><i className="bi bi-instagram"></i></a>
+                    <a href=""><i className="bi bi-linkedin"></i></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-lg-4 col-md-6 d-flex align-items-stretch">
+              <div className="member">
+                <img src="assets/img/trainers/trainer-2.jpg" className="img-fluid" alt="" />
+                <div className="member-content">
+                  <h4>Sarah Jhinson</h4>
+                  <span>Marketing</span>
+                  <p>
+                    Repellat fugiat adipisci nemo illum nesciunt voluptas repellendus. In architecto rerum rerum temporibus
+                  </p>
+                  <div className="social">
+                    <a href=""><i className="bi bi-twitter"></i></a>
+                    <a href=""><i className="bi bi-facebook"></i></a>
+                    <a href=""><i className="bi bi-instagram"></i></a>
+                    <a href=""><i className="bi bi-linkedin"></i></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-lg-4 col-md-6 d-flex align-items-stretch">
+              <div className="member">
+                <img src="assets/img/trainers/trainer-3.jpg" className="img-fluid" alt="" />
+                <div className="member-content">
+                  <h4>William Anderson</h4>
+                  <span>Content</span>
+                  <p>
+                    Voluptas necessitatibus occaecati quia. Earum totam consequuntur qui porro et laborum toro des clara
+                  </p>
+                  <div className="social">
+                    <a href=""><i className="bi bi-twitter"></i></a>
+                    <a href=""><i className="bi bi-facebook"></i></a>
+                    <a href=""><i className="bi bi-instagram"></i></a>
+                    <a href=""><i className="bi bi-linkedin"></i></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </section>
+    </div >
   );
 }
