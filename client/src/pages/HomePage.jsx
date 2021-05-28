ae89f1ee5d035c91ea79569e2461b0edac7f24c9
Hossam Khalil <hossamkhalil001@gmail.com>
2021-05-28 23:05:52 +0200

    feature(landing-page): added popular books section

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,7 +1,44 @@
+import { useEffect, useState } from "react";
+import { NavLink } from "react-router-dom";
+import { hostUrl } from "../api/urls";
 import Footer from "../components/layouts/Footer";
 import Navbar from "../components/layouts/Navbar";
+import { getAuthors } from "../services/authorsService";
+import { getBooks } from "../services/booksService";
+import {
+  parsePaginatedResponse
+} from "../utils/pagination";
+
+
 
 export default function HomePage() {
+
+  const [popularAuthors, setPopularAuthors] = useState([]);
+  const [popularBooks, setPopularBooks] = useState([]);
+
+  useEffect(() => {
+
+    const getAuthorsData = async () => {
+      // get the new page from api
+      const { data } = parsePaginatedResponse(
+        await getAuthors()
+      );
+      setPopularAuthors(data.splice(0, 3));
+    };
+
+    const getBooksData = async () => {
+      // get the new page from api
+      const { data } = parsePaginatedResponse(
+        await getBooks()
+      );
+      setPopularBooks(data.splice(0, 3));
+    };
+
+    getAuthorsData();
+    getBooksData();
+
+  }, [])
+
   return (
     // <Grid container alignItems="flex-start" justify="center" spacing={2}>
     //   <h1>landing</h1>
@@ -9,7 +46,6 @@ export default function HomePage() {
     // </Grid>
     <div>
       <Navbar />
-
       <div className="main-content">
         <section id="hero" className="d-flex justify-content-center align-items-center"
           style={{
@@ -33,95 +69,56 @@ export default function HomePage() {
           <div className="container" data-aos="fade-up">
 
             <div className="section-title">
-              <h2>Courses</h2>
-              <p>Popular Courses</p>
+              <h2>Books</h2>
+              <p>Popular Books</p>
             </div>
 
             <div className="row" data-aos="zoom-in" data-aos-delay="100">
+              {
+                popularBooks.map((book) => {
+                  return (
+                    <div className="col-lg-4 col-md-6 d-flex align-items-stretch" key={book._id}>
+                      <div className="course-item">
+                        <img src={`${hostUrl}${book.coverImage}`} className="img-fluid" alt="..." />
+                        <div className="course-content">
+                          <div className="d-flex justify-content-between align-items-center mb-3">
+                            <h4>{book.categories[0].label}</h4>
+                          </div>
 
-              <div className="col-lg-4 col-md-6 d-flex align-items-stretch">
-                <div className="course-item">
-                  <img src="assets/img/course-1.jpg" className="img-fluid" alt="..." />
-                  <div className="course-content">
-                    <div className="d-flex justify-content-between align-items-center mb-3">
-                      <h4>Web Development</h4>
-                      <p className="price">$169</p>
-                    </div>
-
-                    <h3><a href="course-details.html">Website Design</a></h3>
-                    <p>Et architecto provident deleniti facere repellat nobis iste. Id facere quia quae dolores dolorem tempore.</p>
-                    <div className="trainer d-flex justify-content-between align-items-center">
-                      <div className="trainer-profile d-flex align-items-center">
-                        <img src="assets/img/trainers/trainer-1.jpg" className="img-fluid" alt="" />
-                        <span>Antonio</span>
-                      </div>
-                      <div className="trainer-rank d-flex align-items-center">
-                        <i className="bx bx-user"></i>&nbsp;50&nbsp;&nbsp;
-                        <i className="bx bx-heart"></i>&nbsp;65
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-              <div className="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-md-0">
-                <div className="course-item">
-                  <img src="assets/img/course-2.jpg" className="img-fluid" alt="..." />
-                  <div className="course-content">
-                    <div className="d-flex justify-content-between align-items-center mb-3">
-                      <h4>Marketing</h4>
-                      <p className="price">$250</p>
-                    </div>
-
-                    <h3><a href="course-details.html">Search Engine Optimization</a></h3>
-                    <p>Et architecto provident deleniti facere repellat nobis iste. Id facere quia quae dolores dolorem tempore.</p>
-                    <div className="trainer d-flex justify-content-between align-items-center">
-                      <div className="trainer-profile d-flex align-items-center">
-                        <img src="assets/img/trainers/trainer-2.jpg" className="img-fluid" alt="" />
-                        <span>Lana</span>
-                      </div>
-                      <div className="trainer-rank d-flex align-items-center">
-                        <i className="bx bx-user"></i>&nbsp;35
-                        &nbsp;&nbsp;
-                        <i className="bx bx-heart"></i>&nbsp;42
+                          <h3><NavLink activeClassName="active"
+                            to={`/book/${book._id}`}
+                            exact>
+                            {book.title}
+                          </NavLink></h3>
+                          <p>{book.description}</p>
+                          <div className="trainer d-flex justify-content-between align-items-center">
+                            <div className="trainer-profile d-flex align-items-center">
+                              <img src={`${hostUrl}${book.authors[0].photo}`} className="img-fluid" alt="" />
+                              <NavLink activeClassName="active"
+                                to={`/author/${book.authors[0]._id}`}
+                                exact>
+                                <span>{book.authors[0].firstName + ' ' + book.authors[0].lastName} </span>
+                              </NavLink>
+                            </div>
+                          </div>
+                        </div>
                       </div>
                     </div>
-                  </div>
-                </div>
-              </div>
-
-              <div className="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-lg-0">
-                <div className="course-item">
-                  <img src="assets/img/course-3.jpg" className="img-fluid" alt="..." />
-                  <div className="course-content">
-                    <div className="d-flex justify-content-between align-items-center mb-3">
-                      <h4>Content</h4>
-                      <p className="price">$180</p>
-                    </div>
-
-                    <h3><a href="course-details.html">Copywriting</a></h3>
-                    <p>Et architecto provident deleniti facere repellat nobis iste. Id facere quia quae dolores dolorem tempore.</p>
-                    <div className="trainer d-flex justify-content-between align-items-center">
-                      <div className="trainer-profile d-flex align-items-center">
-                        <img src="assets/img/trainers/trainer-3.jpg" className="img-fluid" alt="" />
-                        <span>Brandon</span>
-                      </div>
-                      <div className="trainer-rank d-flex align-items-center">
-                        <i className="bx bx-user"></i>&nbsp;20
-                            &nbsp;&nbsp;
-                            <i className="bx bx-heart"></i>&nbsp;85
-                    </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
+                  );
+                })
+              }
             </div>
           </div>
 
         </section>
 
         <section id="trainers" className="trainers">
+
           <div className="container" data-aos="fade-up">
+            <div className="section-title">
+              <h2>Authors</h2>
+              <p>Popular Authors</p>
+            </div>
             <div className="row" data-aos="zoom-in" data-aos-delay="100">
               <div className="col-lg-4 col-md-6 d-flex align-items-stretch">
                 <div className="member">
