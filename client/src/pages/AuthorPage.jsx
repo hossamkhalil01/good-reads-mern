2d6fee9a0d5ef54eb8b1155611fc4b632904a558
SarahOuf <sarah.m.ouf@gmail.com>
2021-05-29 03:47:04 +0200

    Feature(author-paage): displayed author's books

--- client/src/pages/AuthorPage.jsx
+++ client/src/pages/AuthorPage.jsx
@@ -1,11 +1,16 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import { hostUrl } from "../api/urls";
+import AvgRating from "../components/AvgRate";
 import Footer from "../components/layouts/Footer";
 import Navbar from "../components/layouts/Navbar";
 import Paginator from "../components/Paginator";
+import UserBookStatus from "../components/UserBookStatus";
+import UserRating from "../components/UserRate";
 import { getAuthor } from "../services/authorsService";
+import { currentUser } from "../services/authService";
 import { getAuthorBooks } from "../services/booksService";
+import { getUser } from "../services/userService";
 import "../styles/AuthorPage.css";
 import {
   createPaginationParams,
@@ -18,6 +23,7 @@ export const AuthorPage = () => {
   const [author, setAuthor] = useState({});
   const [books, setBooks] = useState([]);
   const [pagination, setPagination] = useState({ page: 1, totalPages: 1 });
+  const [updatedUser, setUpdatedUser] = useState({});
 
   const handlePageChange = async (newPage) => {
     // construct the params
@@ -37,10 +43,26 @@ export const AuthorPage = () => {
     console.log(data);
   };
 
+  const getUpdatedUser = async (userId) => {
+    const data = await getUser(userId);
+
+    setUpdatedUser(data.data.data);
+  };
+
   const retriveAuthor = async (authorId) => {
     const data = await getAuthor(authorId);
     console.log(data.data.data);
     setAuthor(data.data.data);
+  };
+
+  const getUserBook = (book) => {
+    const shelf = updatedUser.shelf;
+    const userBook = shelf.find((userBook) => {
+      return userBook.book == book._id;
+    });
+    console.log(userBook);
+    if (userBook) return userBook.status;
+    return;
   };
 
   const retriveAuthorbooks = async (authorId) => {
@@ -52,6 +74,7 @@ export const AuthorPage = () => {
     retriveAuthor(id);
     // retriveAuthorbooks(id);
     handlePageChange();
+    getUpdatedUser(currentUser._id);
   }, [id]);
 
   return (
@@ -84,11 +107,68 @@ export const AuthorPage = () => {
             )}
           </div>
         </div>
-        <div className="row">
-          <Paginator
-            paginationInfo={pagination}
-            onPageChange={handlePageChange}
-          />
+        <div className="row justify-content-center mt-2">
+          <div className="col-md-3 text-center">
+            <h2>Author's Books</h2>
+          </div>
+        </div>
+        {!books.length ? (
+          <div className="row justify-content-center mt-3">
+            <div className="col-md-6">
+              <div className="alert alert-info">
+                No books for this author yet.
+              </div>
+            </div>
+          </div>
+        ) : (
+          ""
+        )}
+        <div className="mt-5">
+          {books.map((book) => (
+            <div>
+              <div key={book?._id} className="row mb-3">
+                <div className="col-md-4">
+                  <img
+                    className="author-book-img"
+                    src={`${hostUrl}${book?.coverImage}`}
+                    alt=""
+                  />
+                </div>
+                <div className="col-md-4">
+                  <h5>{capitalize(book?.title)}</h5>
+                  <AvgRating bookId={book?._id} />
+                </div>
+                <div className="col-md-4">
+                  {updatedUser?.shelf && book?.title ? (
+                    <UserBookStatus
+                      bookId={book?._id}
+                      status={getUserBook(book)}
+                      onStatusChange={() => {}}
+                    />
+                  ) : (
+                    ""
+                  )}
+                  <br />
+                  <UserRating bookId={book?._id} />
+                </div>
+              </div>
+              <div className="row justify-content-center mb-3">
+                <div className="col-md-6">
+                  <hr />
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="row justify-content-center">
+          <div className="col-md-6">
+            <div className="pagination-component">
+              <Paginator
+                paginationInfo={pagination}
+                onPageChange={handlePageChange}
+              />
+            </div>
+          </div>
         </div>
       </div>
       <Footer />
